31a050de183ce212aabf1cc885d92857ed78bc85
georgealexandruiancu <iancugeorgealexandru@gmail.com>
2018-12-14 07:32:12 +0200

    GA and Homework3 working

---
+++ geneticAlgorithm.py
@@ -0,0 +1,15 @@
+import random
+import numpy as np
+
+def initialize(p_zero, N):
+    map = np.zeros((N,N))
+    for i in range(0, N):
+        for j in range(0, i-1):
+            row = []
+            if random.random() > p_zero:
+                map[i][j] = random.random() * 100
+                map[j][i] = map[i][j]
+    print(map)
+    return map
+
+initialize(0.3, 4)
